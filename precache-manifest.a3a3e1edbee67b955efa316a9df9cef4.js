8a9aea0f73e3b71816011a3f6d1732878fc50870
Arpi Beshlikyan <aybeshlikyan@g.ucla.edu>
2020-12-16 12:13:27 -0800

    Updates

---
+++ precache-manifest.a3a3e1edbee67b955efa316a9df9cef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82189ce498887fceec2436ba0c191f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40d9161c18aaa1c7fdd4",
+    "url": "/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "b59aa47c0d245cb8cf96",
+    "url": "/static/css/main.8129708e.chunk.css"
+  },
+  {
+    "revision": "40d9161c18aaa1c7fdd4",
+    "url": "/static/js/2.77396388.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.77396388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b59aa47c0d245cb8cf96",
+    "url": "/static/js/main.2adf797c.chunk.js"
+  },
+  {
+    "revision": "a41c4404f03ccd63b77c",
+    "url": "/static/js/runtime-main.5812fdcd.js"
+  }
+]);
